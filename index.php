2a0001e397310265641b444f0a0fafbda9703e5b
osbjmg <osbjmg@gmail.com>
2016-01-11 18:06:30 -0800

    This document is now successfully checked as HTML 4.01 Transitional!

--- index.php
+++ index.php
@@ -3,8 +3,11 @@ $filename = 'ANET.txt';
 $filepath = 'bin/'.$filename;
 $sixty = 60.00;
 ?>
-<html>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
+   "http://www.w3.org/TR/html4/loose.dtd">
+<html lang="en"> 
  <head>
+ <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
   <title>Is ANET below $60?</title>
   <link href="styles.css" rel="stylesheet" type="text/css">
  </head>
@@ -12,7 +15,7 @@ $sixty = 60.00;
   <div id=top align=center>
    <h1>Is ANET below $60?</h1>
   </div>
-  <div id=middle align=middle>
+  <div id=middle  align=center>
    <br>
    <br>
    <br>
@@ -39,19 +42,19 @@ $sixty = 60.00;
 
    if($price < $sixty) {
        echo "<b><font size=128 color=red>YES</font></b> <br><br>";
-       echo "<img src=images/m.chandler.jpg><br><br><br>";
+       echo "<img src='images/m.chandler.jpg'><br><br><br>";
        echo "ANET <a href=https://www.google.com/finance?q=anet target=_blank>$".$price."</a>";
    }
    else {
        echo "<b><font size=128 color=green>NO</font></b> <br><br>";
-       echo "<img src=images/kenneth_duda.jpg><br><br><br>";
-       echo "<h2><font size=100>ANET <a href=https://www.google.com/finance?q=anet target=_blank>$".$price."</a></font></h2>";
+       echo "<img src='images/kenneth_duda.jpg' alt='Kenneth Duda'><br><br><br>";
+       echo "<h2><font size=100>ANET <a href='https://www.google.com/finance?q=anet' target=_blank>$".$price."</a></font></h2>";
    }
 
    #echo "<br><br><br><br>";
 
    #echo "<div align=right>";
-   echo "<font id=tiny color=#ccc>*as of ".$theTime."</font>"; 
+   echo "<font id=tiny color='#ccc'>*as of ".$theTime."</font>"; 
    #echo "</div>";    
    ?>
 
