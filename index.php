9e11c52a218ef843a102aeab1229a4a9c6815639
osbjmg <osbjmg@gmail.com>
2016-01-20 10:46:52 -0800

    Fixing issue #2 by adding some meta tags.

--- index.php
+++ index.php
@@ -3,11 +3,47 @@ $filename = 'ANET.txt';
 $filepath = 'bin/'.$filename;
 $sixty = 60.00;
 ?>
+<?php
+if (file_exists($filepath)) {
+    $handle	= fopen($filepath, "r");
+    $i = 0;
+    while (!feof($handle)) {
+        $line=fgets($handle);
+        if ($i == 0) {
+            $price = number_format((float) $line, 2);
+        } elseif ($i == 1) {
+            $theTime = $line;
+        } else {
+            echo "Error: source datafile format unrecognized.";
+        }
+        $i++;
+    }
+}
+else {
+     echo "Oops.  I have no clue what ANET is trading at.";
+}
+?>
+
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
    "http://www.w3.org/TR/html4/loose.dtd">
 <html lang="en">
  <head>
  <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+ <meta property="og:site_name" content="isanetbelow60.com">
+
+ <?php
+ if($price < $sixty) {
+     echo '<meta name="description" content="Is ANET below $60? - YES">';
+     echo '<meta property="og:image" content="http://isanetbelow60.com/images/m.chandler.jpg">';
+ }
+
+ else {
+     echo '<meta name="description" content="Is ANET below $60? - NO">';
+     echo '<meta property="og:image" content="http://isanetbelow60.com/images/kenneth_duda.jpg">';
+ }
+ ?>
+
+ <link rel="canonical" href="http://isanetbelow60.com">
  <meta http-equiv="refresh" content="305">
   <title>Is ANET below $60?</title>
   <link href="styles.css" rel="stylesheet" type="text/css">
@@ -21,26 +57,6 @@ $sixty = 60.00;
    <br>
    <br>
    <?php
-
-   if (file_exists($filepath)) {
-       $handle	= fopen($filepath, "r");
-       $i = 0;
-       while (!feof($handle)) {
-           $line=fgets($handle);
-           if ($i == 0) {
-               $price = number_format((float) $line, 2);
-           } elseif ($i == 1) {
-               $theTime = $line;
-           } else {
-               echo "Error: source datafile format unrecognized.";
-           }
-           $i++;
-       }
-   }
-   else {
-        echo "Oops.  I have no clue what ANET is trading at.";
-   }
-
    if($price < $sixty) {
        echo "<b><font size=128 color=red>YES</font></b> <br><br>";
        echo "<img src='images/m.chandler.jpg' alt='A Lawyer'><br><br><br>";
