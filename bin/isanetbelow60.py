3d4693100013f7f03e6ec9ac3506415ea702abf1
osbjmg <osbjmg@gmail.com>
2017-05-17 11:41:11 -0400

    add try: except: for ystockquote.get_historical_prices, as it's failing right now

--- bin/isanetbelow60.py
+++ bin/isanetbelow60.py
@@ -29,14 +29,16 @@ for item in stockInfo:
     print 'Last trade', item['lt_dts']
 print json.dumps(stockInfo ,indent=4)
 '''
-
-# I can ask ystockqote for the previous week/month of closing prices, and plot them
-# let's try the past 30 days
-stockInfo_hist = ystockquote.get_historical_prices('ANET', '2016-05-18', '2016-05-19')
-
 f_rt = open('/home/osbjmg/isanetbelow60.com/bin/STOCK_RT.json', 'w+')
 f_rt.write(json.dumps(stockInfo,indent=4))
 f_rt.close()
-f_hist = open ('/home/osbjmg/isanetbelow60.com/bin/STOCK_HIST.json', 'w+')
-f_hist.write(json.dumps(stockInfo_hist,indent=4))
-f_hist.close()
+
+# I can ask ystockqote for the previous week/month of closing prices, and plot them
+# let's try the past 30 days
+try:
+    stockInfo_hist = ystockquote.get_historical_prices('ANET', '2016-05-18', '2016-05-19')
+    f_hist = open ('/home/osbjmg/isanetbelow60.com/bin/STOCK_HIST.json', 'w+')
+    f_hist.write(json.dumps(stockInfo_hist,indent=4))
+    f_hist.close()
+except:
+    print "Error: ystockquote.get_historical_prices failed."
