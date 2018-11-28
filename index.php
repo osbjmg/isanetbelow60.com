0c00f98a8dd706c75c9d339aa5b0f8efc4edbac8
osbjmg <osbjmg@gmail.com>
2018-11-27 22:24:43 -0500

    adding s to the http

--- index.php
+++ index.php
@@ -138,7 +138,7 @@ if ($displayedImage == 'duda_hug.jpg') {
 
 ?>
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
-   "http://www.w3.org/TR/html4/loose.dtd">
+   "https://www.w3.org/TR/html4/loose.dtd">
 <html lang="en">
  <head>
  <?php
